5d92e6ebcf6ea4ffba1606735bed16526b921935
judewells <judewells@hotmail.com>
2024-06-06 16:14:10 +0100

    configs for examples and main training runs, fix bugs from last merge

--- src/models/mistral_lit_module.py
+++ src/models/mistral_lit_module.py
@@ -11,6 +11,9 @@ class MistralLitModule(LightningModule):
         self.save_hyperparameters(logger=False)
         self.model = MistralForCausalLM(config)
         self.criterion = torch.nn.CrossEntropyLoss()
+        self.train_loss = MeanMetric()
+        self.val_loss = MeanMetric()
+        self.test_loss = MeanMetric()
 
     def forward(self, input_ids, attention_mask=None, labels=None):
         return self.model(input_ids, attention_mask=attention_mask, labels=labels)
@@ -30,7 +33,7 @@ class MistralLitModule(LightningModule):
     def test_step(self, batch: Dict[str, torch.Tensor],  batch_idx: int) -> torch.Tensor:
         outputs = self(batch["input_ids"], batch["attention_mask"], labels=batch["input_ids"])
         loss = outputs.loss
-        self.log("test_loss", loss, on_setp=False, on_epoch=True, prog_bar=False)
+        self.log("test_loss", loss, on_epoch=True, prog_bar=False)
         return loss
 
     def configure_optimizers(self) -> Dict[str, Any]:
