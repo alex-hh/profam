b2f00cc675c501e6345a228700d5108d273601c4
judewells <judewells@hotmail.com>
2024-07-01 22:01:51 +0100

    debugging filepaths clifford scratch

--- submit_scripts/clifford_train.qsub.sh
+++ submit_scripts/clifford_train.qsub.sh
@@ -21,13 +21,14 @@ echo "#################### QSUB SCRIPT START ####################"
 cat "$0" # print the contents of this file to the log
 echo "####################  QSUB SCRIPT END  ####################"
 export ROOT_DIR='/SAN/orengolab/cath_plm/ProFam/profam'
-cd /state && ls && cd partition1 && ls && cd /state/partition1/ProFam_data_on_clifford && ls
-ls /state/partition1/ProFam_data_on_clifford/data
+cd /state && ls && cd partition1 && ls && cd /state/partition1/ProFam_data && ls
+echo "pwd: $(pwd)" && echo "ls: $(ls)"
+cd $ROOT_DIR
 conda activate venvPF
 source /share/apps/source_files/python/python-3.11.9.source
 python ${ROOT_DIR}/src/train.py \
 data=data \
-data.data_dir=/state/patition1/ProFam_data_on_clifford/data \
+data.data_dir=/state/patition1/ProFam_data \
 data.batch_size=100 \
 trainer=gpu \
 trainer.devices=2 \
