edff25a4d418ebce8c9b287e2d0ae7e41151ef1d
judewells <judewells@hotmail.com>
2024-07-25 11:21:19 +0100

    fix path

--- data_creation_scripts/final_create_foldseek.py
+++ data_creation_scripts/final_create_foldseek.py
@@ -1,7 +1,23 @@
 import glob
 import os
-from src.data_creation_scripts.create_foldseek import fasta_to_parquet
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
 
+def fasta_to_parquet(save_dir, batch_id):
+    fastas = glob.glob(save_dir + "*.fasta")
+    results = []
+    for fasta in fastas:
+        with open(fasta, "r") as f:
+            text = f.read()
+        results.append({'text': text})
+        os.remove(fasta)
+    df = pd.DataFrame(results)
+    table = pa.Table.from_pandas(df)
+    output_file = f'{save_dir}/{batch_id}.parquet'
+    pq.write_table(table, output_file)
+    print(f"Saved batch {batch_id} to {output_file}")
+    return output_file
 
 if __name__ == "__main__":
     save_dir = "/SAN/orengolab/cath_plm/ProFam/data/foldseek/"
