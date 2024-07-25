0a7e8c235e056cd7464f64b84e2718a4942fc2b6
judewells <judewells@hotmail.com>
2024-07-25 11:42:49 +0100

    grep search

--- scripts/refactor_missing_seqs.py
+++
@@ -1,8 +0,0 @@
-fpath = "/SAN/orengolab/cath_plm/ProFam/data/foldseek/failed_sequences.txt"
-with open(fpath, 'r') as f:
-    lines = f.readlines()
-unique_lines = list(set(lines))
-
-with open(fpath.replace(".txt", "unique.txt"), "w") as f:
-    for line in unique_lines:
-        f.write(line)
