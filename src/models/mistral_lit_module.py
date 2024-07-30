f2590ece0349120b11277f1f3374453f593738e0
alex-hh <alex.hhooker@gmail.com>
2024-07-30 16:41:39 +0100

    use wrapper with mistral

--- src/models/mistral_lit_module.py
+++ src/models/mistral_lit_module.py
@@ -1,18 +1,15 @@
 from typing import Optional
 
-from transformers import PreTrainedTokenizerFast
+from transformers import PreTrainedTokenizerFast, MistralConfig, MistralForCausalLM
 
 from src.models.base import BaseFamilyLitModule
-from src.models.transformer_mods.modelling_mistral_pflm import (
-    MistralConfigPFLM,
-    MistralForCausalPFLM,
-)
+from src.models.wrapper import TransformerWithSequencePositionEmbeddings
 
 
 class MistralLitModule(BaseFamilyLitModule):
     def __init__(
         self,
-        config: MistralConfigPFLM,
+        config: MistralConfig,
         tokenizer: PreTrainedTokenizerFast,
         lr: float = 1e-4,
         scoring_max_tokens: int = 8000,
@@ -21,8 +18,20 @@ class MistralLitModule(BaseFamilyLitModule):
         scheduler_name: Optional[str] = None,
         num_warmup_steps: int = 1000,
         num_training_steps: Optional[int] = None,
+        use_seq_pos: bool = False,
+        max_seq_pos: int = 2048,
     ) -> None:
-        model = MistralForCausalPFLM(config)
+        model = MistralForCausalLM(config)
+        if (
+            use_seq_pos
+        ):
+            model = TransformerWithSequencePositionEmbeddings(
+                model,
+                model.embed_tokens,
+                embedding_dim=config.hidden_size,
+                use_seq_pos=use_seq_pos,
+                max_seq_pos=max_seq_pos,
+            )
         super().__init__(
             model,
             tokenizer,
