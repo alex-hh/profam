9627d12ea5970efd94761858495de64d74bd2cf8
alex-hh <alex.hhooker@gmail.com>
2024-08-02 13:29:10 +0100

    create foldseek struct submit script

---
+++ submit_scripts/create_foldseek_struct.qsub.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#$ -l tmem=48G
+#$ -l h_vmem=48G
+#$ -l h_rt=3:55:30
+#$ -S /bin/bash
+#$ -N foldseekF
+#$ -o /SAN/orengolab/cath_plm/ProFam/qsub_logs/
+#$ -wd ~/profam
+#$ -j y
+date
+# conda activate venvPF
+source ~/source_files/afenv.source
+python3 data_creation_scripts/create_foldseek_struct.py $1
+date
