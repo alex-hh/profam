ddaabe0c4c1a74de28cd7a063af47d58f6099111
alex-hh <alex.hhooker@gmail.com>
2024-08-08 14:23:54 +0100

    add pdb utils file

--- data_creation_scripts/create_foldseek_struct_with_af50.py
+++ data_creation_scripts/create_foldseek_struct_with_af50.py
@@ -15,8 +15,6 @@ from collections import defaultdict
 from typing import List
 import biotite
 from biotite.sequence import ProteinSequence
-from biotite.structure import filter_amino_acids, get_chains, apply_residue_wise
-from biotite.structure.io import pdb, pdbx
 from biotite.structure.residues import get_residues
 import numpy as np
 import sys
@@ -27,10 +25,9 @@ import os
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
-
-
 import zipfile
 import os
+from src.data.structure import get_atom_coords_residuewise, load_structure
 
 
 def make_zip_dictionary():
@@ -48,21 +45,6 @@ def make_zip_dictionary():
             if line_counter % 100000 == 0:
                 print("Processed", line_counter, "lines for cluster dictionary")
     return af2zip
-
-
-def read_mapping():
-    # One thing we need to be careful about is making sure we can map correctly
-    df = dd.read_csv(
-        "/SAN/bioinf/afdb_domain/zipmaker/zip_index",
-        sep="\t",
-        names=["afdb_id", "seq_hash", "zip_file"]
-    )
-    # df["num_parts"] = df["afdb_id"].apply(lambda x: len(x.split("-")))
-    # min_val, max_val, mean_val = dask.compute(df["num_parts"].min(), df["num_parts"].max(), df["num_parts"].mean())
-    df["uniprot_id"] = df["afdb_id"].apply(lambda x: x.split("-")[1], meta=("x", "str"))
-    df = df.persist()
-    df.set_index("uniprot_id")
-    return df
 
 # Read the mapping file to get the pdb to zip file mapping
 af2zip = make_zip_dictionary()
@@ -129,89 +111,6 @@ def make_cluster_dictionary(cluster_path):
             if line_counter % 100000 == 0:
                 print("Processed", line_counter, "lines for cluster dictionary")
     return cluster_dict
-
-
-backbone_atoms = ["N", "CA", "C", "O"]
-
-
-def _filter_atom_names(array, atom_names):
-    return np.isin(array.atom_name, atom_names)
-
-
-def custom_filter_backbone(array):
-    """
-    Filter all peptide backbone atoms of one array.
-
-    N, CA, C and O
-
-    Parameters
-    ----------
-    array : AtomArray or AtomArrayStack
-        The array to be filtered.
-
-    Returns
-    -------
-    filter : ndarray, dtype=bool
-        This array is `True` for all indices in `array`, where an atom
-        is a part of the peptide backbone.
-    """
-
-    return _filter_atom_names(array, backbone_atoms) & filter_amino_acids(array)
-
-
-def get_atom_coords_residuewise(atoms: List[str], struct: biotite.structure.AtomArray):
-    """
-    Source: esm inverse folding
-    Example for atoms argument: ["N", "CA", "C"]
-    """
-    def filterfn(s, axis=None):
-        filters = np.stack([s.atom_name == name for name in atoms], axis=1)
-        sum = filters.sum(0)
-        if not np.all(sum <= np.ones(filters.shape[1])):
-            raise RuntimeError("structure has multiple atoms with same name")
-        index = filters.argmax(0)
-        coords = s[index].coord
-        coords[sum == 0] = float("nan")
-        return coords
-
-    return apply_residue_wise(struct, struct, filterfn)
-
-
-def load_structure(fpath, chain=None):
-    """
-    Modified from esm inverse folding utils to not remove O
-    Args:
-        fpath: filepath to either pdb or cif file
-        chain: the chain id or list of chain ids to load
-    Returns:
-        biotite.structure.AtomArray
-    """
-    if fpath.endswith("cif"):
-        with open(fpath) as fin:
-            pdbxf = pdbx.PDBxFile.read(fin)
-        structure = pdbx.get_structure(pdbxf, model=1)
-    elif fpath.endswith("pdb"):
-        with open(fpath) as fin:
-            pdbf = pdb.PDBFile.read(fin)
-        structure = pdb.get_structure(pdbf, model=1)
-    bbmask = custom_filter_backbone(structure)
-    # bbmask = filter_backbone(structure)
-    structure = structure[bbmask]
-    all_chains = get_chains(structure)
-    if len(all_chains) == 0:
-        raise ValueError("No chains found in the input file.")
-    if chain is None:
-        chain_ids = all_chains
-    elif isinstance(chain, list):
-        chain_ids = chain
-    else:
-        chain_ids = [chain]
-    for chain in chain_ids:
-        if chain not in all_chains:
-            raise ValueError(f"Chain {chain} not found in input file")
-    chain_filter = [a.chain_id in chain_ids for a in structure]
-    structure = structure[chain_filter]
-    return structure
 
 
 def save_pdbs_to_parquet(save_dir, clusters_to_save, cluster_counter, metadata_lookup, verbose=False):
