a9a12c49570a32db8f0b4f5caca829c334b33636
alex-hh <alex.hhooker@gmail.com>
2024-08-08 17:51:17 +0100

    gpu version of af50 struct script

---
+++ submit_scripts/create_foldseek_af50_struct_gpu.qsub.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#$ -l tmem=88G
+#$ -l gpu=true
+#$ -l h_rt=123:55:30
+#$ -S /bin/bash
+#$ -N foldseekF
+#$ -o /SAN/orengolab/cath_plm/ProFam/qsub_logs/
+#$ -wd ~/profam
+#$ -j y
+date
+# conda activate venvPF
+source ~/source_files/afenv.source
+python3 data_creation_scripts/create_foldseek_struct_with_af50.py $1 --parquet_ids 0
+date
+
+# n.b. if i want to restart parallel - start from 10
