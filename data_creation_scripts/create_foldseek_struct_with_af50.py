57e6f062d95149a11a7c74dda90dcac498d97357
alex-hh <alex.hhooker@gmail.com>
2024-08-08 17:17:58 +0100

    focus parallelisation on pdb extraction

--- data_creation_scripts/create_foldseek_struct_with_af50.py
+++ data_creation_scripts/create_foldseek_struct_with_af50.py
@@ -32,7 +32,7 @@ import os
 from src.data.structure import get_atom_coords_residuewise, load_structure
 
 
-def make_af50_dictionary(shared_dict, af50_path):
+def make_af50_dictionary(af50_path):
     line_counter = 0
     with open(af50_path, "r") as f:
         for line in f:
@@ -44,24 +44,25 @@ def make_af50_dictionary(shared_dict, af50_path):
             # n.b. the 2s are duplicates of the other cluster dict
             # n.b. we don't include the representative in its own cluster atm
             if clu_flag == 1:
-                if rep_id not in shared_dict:
-                    shared_dict[rep_id] = []
-                shared_dict[rep_id].append(entry_id)
+                if rep_id not in af50_dict:
+                    af50_dict[rep_id] = []
+                af50_dict[rep_id].append(entry_id)
             line_counter += 1
             if line_counter % 100000 == 0:
                 print("Processed", line_counter, "lines for cluster dictionary")
     return af50_dict
 
 
-def make_zip_dictionary(shared_dict):
+def make_zip_dictionary():
     line_counter = 0
+    af2zip = {}
     with open("/SAN/bioinf/afdb_domain/zipmaker/zip_index", "r") as f:
         for line in f:
             line = line.strip().split("\t")
             afdb_id = line[0]
             uniprot_id = afdb_id.split("-")[1]
             zip_file = line[2]
-            shared_dict[uniprot_id] = (zip_file, afdb_id)
+            af2zip[uniprot_id] = (zip_file, afdb_id)
 
             line_counter += 1
             if line_counter % 100000 == 0:
@@ -115,20 +116,21 @@ def extract_multi_pdb_files(cluster_ids, afdb_ids, zip_filename, output_folder):
     return successes
 
 
-def make_cluster_dictionary(shared_dict, cluster_path):
+def make_cluster_dictionary(cluster_path):
     line_counter = 0
+    cluster_dict = {}
     with open(cluster_path, "r") as f:
         for line in f:
             line = line.strip().split("\t")
             entry_id = line[0]
             rep_id = line[1]
-            if rep_id not in shared_dict:
-                shared_dict[rep_id] = []
-            shared_dict[rep_id].append(entry_id)
+            if rep_id not in cluster_dict:
+                cluster_dict[rep_id] = []
+            cluster_dict[rep_id].append(entry_id)
             line_counter += 1
             if line_counter % 100000 == 0:
                 print("Processed", line_counter, "lines for cluster dictionary")
-    return shared_dict
+    return cluster_dict
 
 
 def save_pdbs_to_parquet(save_dir, clusters_to_save, cluster_counter, metadata_lookup, verbose=False):
@@ -184,6 +186,16 @@ def save_pdbs_to_parquet(save_dir, clusters_to_save, cluster_counter, metadata_l
     return output_file
 
 
+def extract_pdbs(zip_filename, cluster_ids, afdb_ids, save_dir):
+    t0 = time.time()
+    successes = extract_multi_pdb_files(
+        cluster_ids, afdb_ids, zip_filename, save_dir,
+    )
+    t1 = time.time()
+    print("Extracted", len(afdb_ids), "pdbs in", t1 - t0, "seconds", zip_filename, flush=True)
+    return sum(successes), len(successes) - sum(successes)
+
+
 def build_single_parquet(
     parquet_id,
     cluster_ids,
@@ -229,17 +241,22 @@ def build_single_parquet(
                                 "is_af50_representative": False,
                             }
 
-                for zip_filename, _ids in pdb_lookup.items():
-                    cluster_ids = [x[0] for x in _ids]
-                    afdb_ids = [x[1] for x in _ids]
-                    t0 = time.time()
-                    successes = extract_multi_pdb_files(
-                        cluster_ids, afdb_ids, zip_filename, save_dir,
-                    )
-                    t1 = time.time()
-                    print("Extracted", len(afdb_ids), "pdbs in", t1 - t0, "seconds", zip_filename, flush=True)
-                    seq_success_counter += sum(successes)
-                    seq_fail_counter += len(successes) - sum(successes)
+                # Parallel extraction of pdb files
+                with multiprocessing.Pool() as pool:
+                    results = []
+                    for zip_filename, _ids in pdb_lookup.items():
+                        cluster_ids = [x[0] for x in _ids]
+                        afdb_ids = [x[1] for x in _ids]
+                        result = pool.apply_async(
+                            extract_pdbs,
+                            args=(zip_filename, cluster_ids, afdb_ids, save_dir)
+                        )
+                        results.append(result)
+
+                    for result in results:
+                        success_count, fail_count = result.get()
+                        seq_success_counter += success_count
+                        seq_fail_counter += fail_count
                 print("\nProcessed", cluster_counter, "clusters")
                 print("Number of failed sequences:", seq_fail_counter)
                 print("Number of successful sequences:", seq_success_counter, flush=True)
@@ -277,16 +294,16 @@ def create_foldseek_parquets(
                 skip_af50=skip_af50,
             )
     else:
-        raise NotImplementedError("Parallelisation not yet implemented")
-        # build_single_parquet(
-        #     parquet_id,
-        #     cluster_ids=clusters_to_save[parquet_id],
-        #     af2zip=af2zip,
-        #     af50_dict=af50_dict,
-        #     save_dir=save_dir,
-        #     minimum_cluster_size=minimum_cluster_size,
-        #     skip_af50=skip_af50,
-        # )
+        for parquet_id in parquet_ids:
+            build_single_parquet(
+                parquet_id,
+                cluster_ids=clusters_to_save[parquet_id],
+                af2zip=af2zip,
+                af50_dict=af50_dict,
+                save_dir=save_dir,
+                minimum_cluster_size=minimum_cluster_size,
+                skip_af50=skip_af50,
+            )
 
 
 if __name__ == "__main__":
@@ -296,22 +313,21 @@ if __name__ == "__main__":
     parser.add_argument("--cluster_start", type=int, default=0)
     parser.add_argument("--cluster_end", type=int, default=None)
     parser.add_argument("--parquet_ids", type=int, default=None, nargs="+")
+    parser.add_argument("--skip_af50", action="store_true")
     parser.add_argument("--num_processes", type=int, default=1)
     args = parser.parse_args()
+
+    print("Num cpus", os.cpu_count())
+
     save_dir = "/SAN/orengolab/cath_plm/ProFam/data/foldseek_struct/"
     cluster_dict_pickle_path = os.path.join(save_dir, "foldseek_cluster_dict.pkl")
     af50_dict_pickle_path = os.path.join(save_dir, "af50_cluster_dict.pkl")
 
-    manager = multiprocessing.Manager()
-    cluster_dict = manager.dict()
-    af2zip = manager.dict()
-    af50_dict = manager.dict()
-
-    make_zip_dictionary(af2zip)
+    af2zip = make_zip_dictionary()
 
     if not os.path.exists(cluster_dict_pickle_path):
         print("Creating foldseek dataset", flush=True)
-        make_cluster_dictionary(cluster_dict, args.cluster_path)
+        cluster_dict = make_cluster_dictionary(args.cluster_path)
         print("Number of clusters:", len(cluster_dict))
         print("Saving cluster dictionary")
         with open(save_dir + "foldseek_cluster_dict.pkl", "wb") as f:
@@ -323,7 +339,7 @@ if __name__ == "__main__":
         print("Number of clusters:", len(cluster_dict))
 
     if not os.path.exists(af50_dict_pickle_path):
-        af50_dict = make_af50_dictionary(af50_dict, af50_path)
+        af50_dict = make_af50_dictionary(af50_path)
         with open(af50_dict_pickle_path, "wb") as f:
             pickle.dump(af50_dict, f)
     else:
