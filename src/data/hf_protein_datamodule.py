380eb4d8f47db4e5869c2d2b4cc182093918d2b4
judewells <judewells@hotmail.com>
2024-08-08 19:13:14 +0100

    reformatting, remove redundant files

--- src/data/hf_protein_datamodule.py
+++ src/data/hf_protein_datamodule.py
@@ -90,7 +90,9 @@ class ProteinDataModule(LightningDataModule):
                     # https://github.com/huggingface/datasets/pull/5735
                     train_datasets.append(dataset)
                     train_data_weights.append(self.data_weights[data_key])
-            train_data_weights = [w / sum(train_data_weights) for w in train_data_weights]
+            train_data_weights = [
+                w / sum(train_data_weights) for w in train_data_weights
+            ]
             self.train_dataset = interleave_datasets(
                 train_datasets,
                 probabilities=train_data_weights,
