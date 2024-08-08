0ae699120c6d5e0da1864a16ffa0af1d490fba3d
alex-hh <alex.hhooker@gmail.com>
2024-08-08 18:49:02 +0100

    fix data dir

--- submit_scripts/jade_train.sh
+++ submit_scripts/jade_train.sh
@@ -25,4 +25,4 @@ cd ~/ProFam/profam
 mkdir /raid/local_scratch/$SLURM_JOB_USER/$SLURM_JOB_ID/data
 scp -r ~/ProFam/profam/data/pfam /raid/local_scratch/$SLURM_JOB_USER/$SLURM_JOB_ID/data/
 scp -r ~/ProFam/profam/data/ProteinGym /raid/local_scratch/$SLURM_JOB_USER/$SLURM_JOB_ID/data/
-WANDB_MODE="offline" HYDRA_FULL_ERROR=1 python src/train.py experiment=main_pfam +environment=jade
+WANDB_MODE="offline" HYDRA_FULL_ERROR=1 python src/train.py experiment=main_pfam +environment=jade data.data_dir=/raid/local_scratch/$SLURM_JOB_USER/$SLURM_JOB_ID/data
