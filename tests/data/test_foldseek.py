4332a6f69ed51ddcd053806e10874f67b77a0b91
alex-hh <alex.hhooker@gmail.com>
2024-08-16 10:11:17 +0100

    fix foldseek loading test: run on multiple clusters

--- tests/data/test_foldseek.py
+++ tests/data/test_foldseek.py
@@ -7,23 +7,25 @@ from src.data.utils import backbone_coords_from_example
 
 
 @pytest.fixture
-def foldseek_example():
+def foldseek_df():
     """Fixture to load the sample MSA data."""
     df = pd.read_parquet("data/example_data/foldseek_struct/0.parquet")
-    return df.iloc[0].to_dict()
+    return df
 
 
-def test_foldseek_backbone_loading(foldseek_example):
-    backbone_coords = backbone_coords_from_example(foldseek_example)
-    for seq, acc, recons_coords in zip(
-        foldseek_example["sequences"],
-        foldseek_example["accessions"],
-        backbone_coords,
-    ):
-        pdbfile = "data/example_data/foldseek_struct/0/AF-{}-F1-model_v4.pdb".format(
-            acc, acc
-        )
-        structure = load_structure(pdbfile, chain="A")
-        coords = get_atom_coords_residuewise(["N", "CA", "C", "O"], structure)
-        assert np.allclose(coords, recons_coords)
-        assert len(coords) == len(seq)
+def test_foldseek_backbone_loading(foldseek_df):
+    for _, row in foldseek_df.iterrows():
+        foldseek_example = row.to_dict()
+        backbone_coords = backbone_coords_from_example(foldseek_example)
+        for seq, acc, recons_coords in zip(
+            foldseek_example["sequences"],
+            foldseek_example["accessions"],
+            backbone_coords,
+        ):
+            pdbfile = "data/example_data/foldseek_struct/0/AF-{}-F1-model_v4.pdb".format(
+                acc, acc
+            )
+            structure = load_structure(pdbfile, chain="A")
+            coords = get_atom_coords_residuewise(["N", "CA", "C", "O"], structure)
+            assert np.allclose(coords, recons_coords)
+            assert len(coords) == len(seq)
