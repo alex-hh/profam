a91c3367c7c0d2d8349b174ab9aa4557a8cea87d
alex-hh <alex.hhooker@gmail.com>
2024-08-18 23:29:50 +0100

    restore default values to submit script

--- submit_scripts/create_foldseek_noaf50_struct_array.qsub.sh
+++ submit_scripts/create_foldseek_noaf50_struct_array.qsub.sh
@@ -3,7 +3,7 @@
 #$ -l h_vmem=5G
 #$ -l h_rt=25:55:30
 #$ -S /bin/bash
-#$ -t 1-1  # 8000 for 2000000 at 250; 1800/4500 for 450000 at 250/100
+#$ -t 1-1800  # 8000 for 2000000 at 250; 1800/4500 for 450000 at 250/100
 #$ -N foldseekF
 #$ -o /SAN/orengolab/cath_plm/ProFam/qsub_logs/
 #$ -wd ~/profam
@@ -15,6 +15,6 @@ date
 SCRATCH_DIR=/scratch0/$USER/$JOB_ID
 mkdir -p ${SCRATCH_DIR}/data
 source ~/source_files/afenv.source
-python3 data_creation_scripts/create_foldseek_struct_with_af50.py $1 /SAN/orengolab/cath_plm/ProFam/data/foldseek_struct_example  --skip_af50 --num_processes 1 --minimum_foldseek_cluster_size 1 --parquet_ids $((SGE_TASK_ID - 1))
-# TODO: zip the scratch dir?
+python3 data_creation_scripts/create_foldseek_struct_with_af50.py $1 ${SCRATCH_DIR}/data  --skip_af50 --num_processes 1 --minimum_foldseek_cluster_size 10 --parquet_ids $((SGE_TASK_ID - 1))
+rm -rf ${SCRATCH_DIR}/data
 date
