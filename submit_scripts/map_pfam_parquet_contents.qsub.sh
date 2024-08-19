ad1ae03e31ec4be2ac1756c4bceb0a1fa420207d
judewells <judewells@hotmail.com>
2024-08-19 18:33:20 +0100

    submit script

---
+++ submit_scripts/map_pfam_parquet_contents.qsub.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#$ -l tmem=32G
+#$ -l h_vmem=32G
+#$ -l h_rt=47:55:30
+#$ -S /bin/bash
+#$ -N remapPfam
+#$ -t 1
+#$ -o /SAN/orengolab/cath_plm/ProFam/qsub_logs/
+#$ -wd /SAN/orengolab/cath_plm/ProFam/profam
+#$ -j y
+
+date
+hostname
+echo "qsub script: $0"
+echo "#################### QSUB SCRIPT START ####################"
+cat "$0" # print the contents of this file to the log
+echo "####################  QSUB SCRIPT END  ####################"
+
+conda activate venvPF
+
+ROOT_DIR='/SAN/orengolab/cath_plm/ProFam/profam'
+
+cd $ROOT_DIR
+export PYTHONPATH=$PYTHONPATH:$ROOT_DIR
+python ${ROOT_DIR}/scripts/map_pfam_parquet_contents.py --task_index $((SGE_TASK_ID - 1))
+date
+
