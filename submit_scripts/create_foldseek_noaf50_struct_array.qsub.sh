3b54c077aab965725cfc050c39cc58bed3257813
alex-hh <alex.hhooker@gmail.com>
2024-08-19 11:20:23 +0100

    increase memory

--- submit_scripts/create_foldseek_noaf50_struct_array.qsub.sh
+++ submit_scripts/create_foldseek_noaf50_struct_array.qsub.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#$ -l tmem=5G
-#$ -l h_vmem=5G
+#$ -l tmem=10G
+#$ -l h_vmem=10G
 #$ -l h_rt=25:55:30
 #$ -S /bin/bash
 #$ -t 1-1  # 8000 for 2000000 at 250; 1800/4500 for 450000 at 250/100
@@ -16,7 +16,7 @@ SCRATCH_DIR=/scratch0/$USER/$JOB_ID
 mkdir -p ${SCRATCH_DIR}/data
 source ~/source_files/afenv.source
 export PATH=/SAN/orengolab/cath_plm/ProFam/foldmason/bin/:$PATH
-python3 data_creation_scripts/create_foldseek_struct_with_af50.py $1 /SAN/orengolab/cath_plm/ProFam/data/foldseek_struct_example  --skip_af50 --num_processes 1 --minimum_foldseek_cluster_size 10 --parquet_ids $((SGE_TASK_ID - 1)) --run_foldmason
+python3 data_creation_scripts/create_foldseek_struct_with_af50.py $1 /SAN/orengolab/cath_plm/ProFam/data/foldseek_struct_example  --skip_af50 --num_processes 1 --minimum_foldseek_cluster_size 1 --parquet_ids $((SGE_TASK_ID - 1)) --run_foldmason
 # TODO: zip the scratch dir?, remove scratch dir
 rm -rf ${SCRATCH_DIR}/data
 date
