78384abe3c5e956fad92f2f202ba360a10b3dd99
judewells <judewells@hotmail.com>
2024-08-22 10:30:59 +0100

    WIP refactoring

--- src/data/pfam_classification.py
+++ src/data/pfam_classification.py
@@ -28,7 +28,7 @@ from datasets import Dataset
 from torch import arange
 
 import src.data.utils as data_utils
-from src.data import fasta
+from src.data.fasta import convert_sequence_with_positions, read_fasta
 from src.data.family_classification import family_columns
 from src.data.utils import tokenize, tokenize_completions
 
@@ -49,7 +49,7 @@ def tokenize_pfam(
 ):
     """"""
     msa_name = msa_path["msa_paths"].split("/")[-1].split("_")[0]
-    msa_names, msa_seqs = fasta.read_fasta(
+    msa_names, msa_seqs = read_fasta(
         msa_path["msa_paths"],
         keep_insertions=keep_insertions,
         to_upper=to_upper,
@@ -63,6 +63,8 @@ def tokenize_pfam(
         seed=seed,
         keep_first=True,
         drop_first=False,
+
+
         max_tokens=max_msa_tokens,
     )
 
@@ -88,14 +90,6 @@ def tokenize_pfam(
     sample["eval_fam_ids"] = "|".join(eval_names)
     return sample
 
-def process_eval_seqs(
-    eval_path: str,
-    use_seq_pos: bool,
-    keep_insertions,
-    keep_gaps,
-    to_upper,
-):
-    pass
 
 def load_pfam_classification_dataset(
     tokenizer,
@@ -106,6 +100,7 @@ def load_pfam_classification_dataset(
     max_tokens=10000,
     use_seq_pos=False,
     max_seq_pos: int = 1024,
+    use_msa_pos: bool = False,
     seed=42,
     num_workers=4,
     max_eval_per_fam=4,
@@ -119,46 +114,62 @@ def load_pfam_classification_dataset(
     train_names = set([p.split("/")[-1].split("_train.")[0] for p in prompt_seq_paths])
     test_names = set([p.split("/")[-1].split("_test.")[0] for p in eval_seq_paths])
     assert train_names == test_names, "Pfam prompt and completion files do not match."
-    max_eval_len = 0
-    for eval_path in eval_seq_paths:
-        eval_name = eval_path.split("/")[-1].split("_")[0]
-        eval_names, eval_seqs = fasta.read_fasta(
-            eval_path,
+    if use_seq_pos:
+        # don't change the fasta at all here processing done later
+        seq_load_func = partial(
+            read_fasta,
             keep_insertions=True,
             keep_gaps=True,
             to_upper=False,
         )
+    else:
+        seq_load_func = partial(
+            read_fasta,
+            keep_insertions=keep_insertions,
+            keep_gaps=keep_gaps,
+            to_upper=to_upper,
+        )
+    # first load the raw seqs from all the eval files
+    for eval_path in eval_seq_paths:
+        eval_name = eval_path.split("/")[-1].split("_")[0]
+        eval_names, eval_seqs = seq_load_func(eval_path)
         assert len(set(eval_names)) == 1
         assert eval_name in eval_names
         combined_eval_seqs.extend(eval_seqs[:max_eval_per_fam])
         eval_labels.extend(eval_names[:max_eval_per_fam])
-        # todo: current form always counts gaps towards limit
-        max_eval_len = max(max_eval_len, max(map(len, eval_seqs)))
 
+    # process the raw seqs
     if use_seq_pos:
-        completion_seqs = []
-        completion_seq_pos = []
+        combined_eval_seq_pos = []
+        processed_eval_seqs = []
         for seq in combined_eval_seqs:
-            new_seq, pos, is_match = fasta.convert_sequence_with_positions(
+            new_seq, pos, is_match = convert_sequence_with_positions(
                 seq,
-                keep_gaps=True,
-                keep_insertions=True,
-                to_upper=False
-
+                keep_gaps=keep_gaps,
+                keep_insertions=keep_insertions,
+                to_upper=to_upper,
+                use_msa_pos=use_msa_pos,
             )
+            combined_eval_seq_pos.append(pos)
+            processed_eval_seqs.append(new_seq)
+
     else:
-        completion_seqs = combined_eval_seqs
+        processed_eval_seqs = combined_eval_seqs
+    max_eval_len = max([len(s) for s in processed_eval_seqs])
+
     # Tokenize eval sequences once and re-use
     tok_eval_seqs = tokenize_completions(
-        sample={"completion_seqs": combined_eval_seqs},
+        sample={"completion_seqs": processed_eval_seqs},
         tokenizer=tokenizer,
         bos_token="sep",
+        max_seq_pos=max_seq_pos,
+        use_seq_pos=False
     )
     max_msa_tokens = max_tokens - max_eval_len - 2
     assert (tok_eval_seqs["completion_ids"][:, 0] == tokenizer.vocab["[SEP]"]).all()
 
     tokenized_eval_seqs = tok_eval_seqs["completion_ids"]
-    # tokenized_eval_seqs = tokenize_eval_seqs(tokenizer, combined_eval_seqs, use_seq_pos, max_seq_pos)
+
     msa_paths = sorted(glob.glob(f"{pfam_dir}/*_train.fasta"))
 
     process_func = partial(
@@ -168,8 +179,9 @@ def load_pfam_classification_dataset(
         tokenizer=tokenizer,
         max_msa_tokens=max_msa_tokens,
         use_seq_pos=use_seq_pos,
+        use_msa_pos=use_msa_pos,
         max_seq_pos=max_seq_pos,
-        completion_seq_pos=completion_seq_pos,
+        completion_seq_pos=combined_eval_seq_pos,
         keep_insertions=keep_insertions,
         to_upper=to_upper,
         keep_gaps=keep_gaps,
