ed57676580de93df4454b3c5882b4efb7655fcc1
alex-hh <alex.hhooker@gmail.com>
2024-08-23 15:49:21 +0100

    add script to check for missing parquets

---
+++ scripts/check_parquets.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# check for existence of parquets with indices in a given range
+parquet_dir=$1
+max_index=$2
+for i in ${0..$max_index};
+    do if [ ! -f $parquet_dir/$i.parquet ]; then
+        echo "$i"
+    fi
+done
