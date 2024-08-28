f7a24e7b95c8b9bc6caa48f2a0d6355b0c820559
alex-hh <alex.hhooker@gmail.com>
2024-08-28 15:16:46 +0100

    parallel foldseek representatives creation script

---
+++ submit_scripts/create_foldseek_representatives_gpu.qsub.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#$ -l h_rt=58:55:30
+#$ -l gpu=true
+#$ -S /bin/bash
+#$ -N foldseekF
+#$ -o /SAN/orengolab/cath_plm/ProFam/qsub_logs/
+#$ -wd ~/profam
+#$ -j y
+date
+# conda activate venvPF
+SCRATCH_DIR=/scratch0/$USER/$JOB_ID
+mkdir -p ${SCRATCH_DIR}/data
+cp /SAN/bioinf/afdb_domain/zipmaker/zip_index $SCRATCH_DIR/data/
+cp /SAN/orengolab/cath_plm/ProFam/data/afdb/5-allmembers-repId-entryId-cluFlag-taxId.tsv $SCRATCH_DIR/data/
+source ~/source_files/afenv.source
+export PATH=/SAN/orengolab/cath_plm/ProFam/foldseek/bin/:$PATH
+python3 data_creation_scripts/create_foldseek_representatives.py $1 ${SCRATCH_DIR}/data --num_processes 10
+rm -rf ${SCRATCH_DIR}/data
+date
