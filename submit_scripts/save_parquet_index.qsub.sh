0f1ff9221f58b6fd37b0bc1fa2551154ddb26991
alex-hh <alex.hhooker@gmail.com>
2024-08-28 16:59:51 +0100

    parquet index submit script

---
+++ submit_scripts/save_parquet_index.qsub.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#$ -l h_rt=10:55:30
+#$ -l h_vmem=8G
+#$ -l tmem=8G
+#$ -S /bin/bash
+#$ -N foldseekF
+#$ -o /SAN/orengolab/cath_plm/ProFam/qsub_logs/
+#$ -wd ~/profam
+#$ -j y
+date
+# conda activate venvPF
+SCRATCH_DIR=/scratch0/$USER/$JOB_ID
+mkdir -p ${SCRATCH_DIR}/data
+source ~/source_files/afenv.source
+python3 data_creation_scripts/save_parquet_index.py "$@"
+rm -rf ${SCRATCH_DIR}/data
+date
