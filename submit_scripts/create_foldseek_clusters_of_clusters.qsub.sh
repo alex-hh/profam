086d2f50af14b119380929265d212f2c1585562c
alex-hh <alex.hhooker@gmail.com>
2024-08-30 01:42:16 +0100

    fix cluster of clusters submit script

--- submit_scripts/create_foldseek_clusters_of_clusters.qsub.sh
+++ submit_scripts/create_foldseek_clusters_of_clusters.qsub.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#$ -l tmem=10G
-#$ -l h_vmem=10G
-#$ -l h_rt=34:55:30
+#$ -l tmem=30G
+#$ -l h_vmem=30G
+#$ -l h_rt=64:55:30
 #$ -S /bin/bash
 #$ -N aug_foldseek
 #$ -o /SAN/orengolab/cath_plm/ProFam/qsub_logs/
@@ -10,5 +10,5 @@
 date
 # conda activate venvPF
 source ~/source_files/afenv.source
-python3 data_creation_scripts/create_foldseek_with_af50.py "$@"
+python3 data_creation_scripts/create_foldseek_clusters_of_clusters.py "$@"
 date
