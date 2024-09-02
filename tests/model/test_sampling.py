a8dfd2404f7b3433e572c8ded812d1f6cc6beef6
alex-hh <alex.hhooker@gmail.com>
2024-09-02 19:21:42 +0100

    formatting

--- tests/model/test_sampling.py
+++ tests/model/test_sampling.py
@@ -1,5 +1,10 @@
 def test_seqpos_start(default_model, proteingym_batch):
     # TODO: somehow use a dummy pipeline for sampling tests
     # n.b. completion seq pos is b, n, l
-    assert (proteingym_batch["completion_seq_pos"][:,:, 1] == default_model.model.start_seq_pos).all()
-    assert (proteingym_batch["seq_pos"][:, 2] == default_model.model.start_seq_pos).all()
+    assert (
+        proteingym_batch["completion_seq_pos"][:, :, 1]
+        == default_model.model.start_seq_pos
+    ).all()
+    assert (
+        proteingym_batch["seq_pos"][:, 2] == default_model.model.start_seq_pos
+    ).all()
