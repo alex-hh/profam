4afa27d49d08dfc9b5dbbf841084a83caf0afb0f
alex-hh <alex.hhooker@gmail.com>
2024-09-03 17:12:54 +0100

    foldseek af50 struct parallel creation script

---
+++ submit_scripts/create_foldseek_af50_struct_parallel.qsub.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#$ -l h_rt=128:55:30
+#$ -l gpu_type=(a100|a100_80)
+#$ -l gpu=true
+#$ -S /bin/bash
+#$ -N foldseek_af50_parallel
+#$ -o /SAN/orengolab/cath_plm/ProFam/qsub_logs/
+#$ -j y
+#$ -l avx2=yes  # for foldmason
+
+date
+hostname
+SCRATCH_DIR=/scratch0/$USER/$JOB_ID
+mkdir -p ${SCRATCH_DIR}/data
+echo "Created scratch dir"
+
+# copy the datasets to the scratch space
+mkdir -p ${SCRATCH_DIR}/data
+rsync -av /SAN/orengolab/cath_plm/ProFam/profam ${SCRATCH_DIR}/
+cd ${SCRATCH_DIR}/profam
+echo "Copied ProFam to ${SCRATCH_DIR}/profam/"
+
+source /share/apps/source_files/python/python-3.11.9.source
+ROOT_DIR=${SCRATCH_DIR}/profam
+cd $ROOT_DIR
+python3 -m venv ${SCRATCH_DIR}/venv
+# Activate the virtual environment
+source ${SCRATCH_DIR}/venv/bin/activate
+pip install -r ${SCRATCH_DIR}/profam/requirements.txt
+
+
+ls /scratch0/$USER/$JOB_ID
+cp /SAN/bioinf/afdb_domain/zipmaker/zip_index $SCRATCH_DIR/data/
+cp /SAN/orengolab/cath_plm/ProFam/data/afdb/5-allmembers-repId-entryId-cluFlag-taxId.tsv $SCRATCH_DIR/data/
+
+mkdir ${SCRATCH_DIR}/data/foldseek_af50_struct
+export PATH=/SAN/orengolab/cath_plm/ProFam/foldmason/bin/:$PATH
+python3 data_creation_scripts/create_foldseek_struct_with_af50.py $1 ${SCRATCH_DIR}/data --minimum_foldseek_cluster_size 1\
+    --parquet_ids $file_prefix --run_foldmason --num_processes 30 --save_dir $SCRATCH_DIR/data/foldseek_af50_struct
+rsync -av $SCRATCH_DIR/data/foldseek_af50_struct /SAN/orengolab/cath_plm/ProFam/data/
+rm -rf ${SCRATCH_DIR}/data
