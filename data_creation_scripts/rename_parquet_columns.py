04d6e7b31afef2bdfdf791b0fa98d5f6a2e8632b
alex-hh <alex.hhooker@gmail.com>
2024-09-03 14:39:24 +0100

    simplify column renaming

--- data_creation_scripts/rename_parquet_columns.py
+++ data_creation_scripts/rename_parquet_columns.py
@@ -4,7 +4,7 @@ import pandas as pd
 import tqdm
 
 
-def rename_column_in_parquet(file_path, old_column_name, new_column_name, output_dir=None):
+def rename_column_in_parquet(file_path, old_column_name, new_column_name):
     """
     Renames a column in a Parquet file and saves the updated file.
     
@@ -20,21 +20,13 @@ def rename_column_in_parquet(file_path, old_column_name, new_column_name, output
     if old_column_name in df.columns:
         df.rename(columns={old_column_name: new_column_name}, inplace=True)
         print(f"Renamed column '{old_column_name}' to '{new_column_name}' in {file_path}")
+        df.to_parquet(file_path, index=False)
     else:
         print(f"Column '{old_column_name}' not found in {file_path}")
-
-    # Determine the output path
-    if output_dir:
-        os.makedirs(output_dir, exist_ok=True)
-        output_file_path = os.path.join(output_dir, os.path.basename(file_path))
-    else:
-        output_file_path = file_path  # Overwrite the original file
-
-    # Save the updated DataFrame back to Parquet without saving the index
-    df.to_parquet(output_file_path, index=False)
+        return
 
 
-def process_parquet_files(file_list, old_column_name, new_column_name, output_dir=None):
+def process_parquet_files(file_list, old_column_name, new_column_name):
     """
     Processes a list of Parquet files, renaming a specific column in each file.
     
@@ -44,7 +36,7 @@ def process_parquet_files(file_list, old_column_name, new_column_name, output_di
     :param output_dir: Directory where the updated files will be saved. If None, it overwrites the original files.
     """
     for file_path in tqdm.tqdm(file_list):
-        rename_column_in_parquet(file_path, old_column_name, new_column_name, output_dir)
+        rename_column_in_parquet(file_path, old_column_name, new_column_name)
 
 
 # Example usage
@@ -55,4 +47,4 @@ if __name__ == "__main__":
     parquet_files = glob.glob(os.path.join(data_dir, 'foldseek_struct/*.parquet'))
 
     # Rename 'cluster_id' to 'fam_id' in each file
-    process_parquet_files(parquet_files, old_column_name='cluster_id', new_column_name='fam_id', output_dir=None)
+    process_parquet_files(parquet_files, old_column_name='cluster_id', new_column_name='fam_id')
