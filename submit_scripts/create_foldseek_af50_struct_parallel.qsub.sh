397d1113737ca4aef777f4b7c994c4ac461101c1
alex-hh <alex.hhooker@gmail.com>
2024-09-04 11:36:22 +0100

    adjust scripts for clifford

--- submit_scripts/create_foldseek_af50_struct_parallel.qsub.sh
+++ submit_scripts/create_foldseek_af50_struct_parallel.qsub.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#$ -l tmem=128G
+#$ -l h_vmem=128G
 #$ -l h_rt=128:55:30
 #$ -l gpu_type=(a100|a100_80)
 #$ -l gpu=true
@@ -30,7 +32,7 @@ cp /SAN/orengolab/cath_plm/ProFam/data/afdb/5-allmembers-repId-entryId-cluFlag-t
 
 mkdir ${SCRATCH_DIR}/data/foldseek_af50_struct
 export PATH=/SAN/orengolab/cath_plm/ProFam/foldmason/bin/:$PATH
-python3 data_creation_scripts/create_foldseek_struct_with_af50.py $1 ${SCRATCH_DIR}/data --minimum_foldseek_cluster_size 1\
+python3 data_creation_scripts/create_foldseek_struct_with_af50.py $1 ${SCRATCH_DIR}/data --minimum_foldseek_cluster_size 1 \
     --parquet_ids $file_prefix --run_foldmason --num_processes 30 --save_dir $SCRATCH_DIR/data/foldseek_af50_struct
 rsync -av $SCRATCH_DIR/data/foldseek_af50_struct /SAN/orengolab/cath_plm/ProFam/data/
 rm -rf ${SCRATCH_DIR}/data
--- submit_scripts/create_foldseek_representatives_parallel.qsub.sh
+++ submit_scripts/create_foldseek_representatives_parallel.qsub.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+#$ -l tmem=128G
+#$ -l h_vmem=128G
 #$ -l h_rt=128:55:30
-##$ -l gpu_type=(a100|a100_80)
+#$ -l gpu_type=(a100|a100_80)
 #$ -l gpu=true
 #$ -S /bin/bash
 #$ -N foldseek_af50_parallel
@@ -19,13 +21,13 @@ echo "Created scratch dir"
 # cd ${SCRATCH_DIR}/profam
 # echo "Copied ProFam to ${SCRATCH_DIR}/profam/"
 
-source ~/source_files/afenv.source
-cd ~/profam
+# source ~/source_files/afenv.source
+# cd ~/profam
 
-# export ROOT_DIR='/SAN/orengolab/cath_plm/ProFam/profam'
-# cd $ROOT_DIR
-# conda activate venvPF
-# source /share/apps/source_files/python/python-3.11.9.source
+export ROOT_DIR='/SAN/orengolab/cath_plm/ProFam/profam'
+cd $ROOT_DIR
+conda activate venvPF
+source /share/apps/source_files/python/python-3.11.9.source
 
 ls /scratch0/$USER/$JOB_ID
 cp /SAN/bioinf/afdb_domain/zipmaker/zip_index $SCRATCH_DIR/data/
@@ -34,7 +36,6 @@ cp /SAN/orengolab/cath_plm/ProFam/data/afdb/5-allmembers-repId-entryId-cluFlag-t
 mkdir ${SCRATCH_DIR}/data/foldseek_af50_struct
 export PATH=/SAN/orengolab/cath_plm/ProFam/foldmason/bin/:$PATH
 python3 data_creation_scripts/create_foldseek_representatives.py $1 ${SCRATCH_DIR}/data \
-    --num_processes 30 --af50
-# --save_dir $SCRATCH_DIR/data/foldseek_af50_representatives
-# rsync -av $SCRATCH_DIR/data/foldseek_af50_struct /SAN/orengolab/cath_plm/ProFam/data/
+    --num_processes 30 --af50 --save_dir $SCRATCH_DIR/data/foldseek_af50_representatives
+rsync -av $SCRATCH_DIR/data/foldseek_af50_struct /SAN/orengolab/cath_plm/ProFam/data/
 rm -rf ${SCRATCH_DIR}/data
