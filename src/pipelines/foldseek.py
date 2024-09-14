33d1e61836623a0f57ade4540f7ba1b5a24cd9bb
alex-hh <alex.hhooker@gmail.com>
2024-09-14 13:30:46 +0100

    fix preprocessor config

--- src/pipelines/foldseek.py
+++ src/pipelines/foldseek.py
@@ -32,17 +32,3 @@ class FoldseekGenerationsPipeline(ParquetMixin, GenerationsEvaluatorPipeline):
 
     def get_instance_summary(self, instance_id: str) -> Dict[str, float]:
         return {}
-
-    def load_protein_document(self, instance_id: str):
-        protein_example = self.get_protein_example(instance_id)
-        # TODO: use prompt_builder.preprocessor build_document? with max_tokens=None, shuffle=False should be ok.
-        return ProteinDocument(
-            identifier=instance_id,
-            sequences=protein_example["sequences"],
-            accessions=protein_example["accessions"],
-            backbone_coords=backbone_coords_from_example(protein_example),
-            plddts=protein_example["plddts"],
-            structure_tokens=[
-                s.replace("-", "").lower() for s in protein_example["msta_3di"]
-            ],
-        )
