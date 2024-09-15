fix passing custom attention mask

src/models/llama.py
@@ -63,14 +63,19 @@ def _prepare_4d_causal_attention_mask_with_cache_position(
         # N.B. the combination of binary and non-binary masks in the original code here is pretty confusing.
         # we try to first build required binary mask, then convert to a bias mask.
         causal_mask = (
-            torch.arange(target_length, device=device) <= cache_position.reshape(-1, 1)
+            torch.arange(target_length, device=device) <= cache_position.unsqueeze(1)
         )[None].expand(
             batch_size, -1, -1
         )  # sequence_length, target_length
         if attention_mask is not None:
-            causal_mask[:, :, : attention_mask.shape[-1]] &= attention_mask[
-                :, None, :
-            ].bool()
+            if attention_mask.shape[-1] < target_length:
+                full_attention_mask = torch.ones(
+                    batch_size, target_length, device=device
+                )
+                full_attention_mask[:, : attention_mask.shape[-1]] = attention_mask
+            else:
+                full_attention_mask = attention_mask
+            causal_mask = causal_mask & full_attention_mask[:, None, :].bool()
         causal_mask = causal_mask[:, None]  # add head dim
     elif attention_mask.isin([0, 1]).all() and not (attention_mask == 0).all():
         # if we pass all 0s there is ambiguity, but we assume it means a bias mask, since it would prevent any attention.

tests/model/test_attention_masking.py
@@ -12,6 +12,7 @@ def test_prepare_4d_causal_attention_mask_with_cache_position():
     sequence_length = 5
     target_length = 10
     past_seen_length = target_length - sequence_length
+    batch_size = 3
     cache_position = torch.arange(past_seen_length, past_seen_length + sequence_length)
     our_mask = mod_prepare_4d_causal_attention_mask_with_cache_position(
         attention_mask=None,
@@ -20,7 +21,7 @@ def test_prepare_4d_causal_attention_mask_with_cache_position():
         cache_position=cache_position,
         dtype=torch.float32,
         device=torch.device("cpu"),
-        batch_size=3,
+        batch_size=batch_size,
         min_dtype=torch.finfo(torch.float32).min,
     )
 
@@ -40,20 +41,22 @@ def test_prepare_4d_causal_attention_mask_with_cache_position():
     sequence_length = 14
     target_length = 14
     past_seen_length = target_length - sequence_length
+    attention_mask = torch.ones(batch_size, target_length)
+    attention_mask[:, -5:] = 0  # simulate padding
     cache_position = torch.arange(past_seen_length, past_seen_length + sequence_length)
     our_mask = mod_prepare_4d_causal_attention_mask_with_cache_position(
-        attention_mask=None,
+        attention_mask=attention_mask,
         sequence_length=sequence_length,
         target_length=target_length,
         cache_position=cache_position,
         dtype=torch.float32,
         device=torch.device("cpu"),
-        batch_size=3,
+        batch_size=batch_size,
         min_dtype=torch.finfo(torch.float32).min,
     )
 
     llama_mask = _prepare_4d_causal_attention_mask_with_cache_position(
-        attention_mask=None,
+        attention_mask=attention_mask,
         sequence_length=sequence_length,
         target_length=target_length,
         cache_position=cache_position,