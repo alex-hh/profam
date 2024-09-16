61a35627ef1c0e6911c165dc39e3dfc3253c8d05
alex-hh <alex.hhooker@gmail.com>
2024-09-16 10:51:49 +0100

    verbose single submit

--- submit_scripts/create_foldseek_af50_struct_from_db_job_list.sh
+++ submit_scripts/create_foldseek_af50_struct_from_db_job_list.sh
@@ -7,5 +7,6 @@ hostname
 while IFS= read -r task_id; do
   # Execute the SUBMIT JOB command (replace it with the actual command)
     file_prefix=$(($task_id - 1))
+    echo Submitting job to create parquet $file_prefix
     qsub ~/profam/submit_scripts/create_foldseek_af50_struct_from_db_single.qsub.sh $file_prefix
 done < $1
