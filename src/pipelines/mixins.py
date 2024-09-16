7cc6df1f59f335ea2f4c1df951a1d83d57cb10d3
alex-hh <alex.hhooker@gmail.com>
2024-09-16 18:03:17 +0100

    evaluations pipeline related configs

--- src/pipelines/mixins.py
+++ src/pipelines/mixins.py
@@ -1,6 +1,8 @@
-from typing import Optional
+from typing import Dict, Optional
 
 import pandas as pd
+
+from src.pipelines.pipeline import GenerationsEvaluatorPipeline
 
 
 class ParquetMixin:
@@ -71,3 +73,31 @@ class ParquetMixin:
             evaluation_df = pd.read_parquet(parquet_file)
         dict = evaluation_df.loc[instance_id].to_dict()
         return dict
+
+
+class ParquetGenerationsPipeline(ParquetMixin, GenerationsEvaluatorPipeline):
+    def __init__(
+        self,
+        *args,
+        cluster_id_col: str = "fam_id",
+        evaluation_parquet: str = None,
+        evaluation_accessions_file: str = None,
+        parquet_index: str = None,
+        evaluation_accessions: list = None,
+        **kwargs,
+    ):
+        super().__init__(
+            *args,
+            instance_id_col=cluster_id_col,
+            evaluation_parquet=evaluation_parquet,
+            evaluation_accessions_file=evaluation_accessions_file,
+            evaluation_accessions=evaluation_accessions,
+            parquet_index=parquet_index,
+            **kwargs,
+        )
+
+    def instance_ids(self):
+        return self.evaluation_accessions
+
+    def get_instance_summary(self, instance_id: str) -> Dict[str, float]:
+        return {}
