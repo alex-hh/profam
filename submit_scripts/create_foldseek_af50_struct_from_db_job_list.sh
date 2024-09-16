5715d9afb11ba1fe6d33d600a224408749ea6838
alex-hh <alex.hhooker@gmail.com>
2024-09-16 10:18:54 +0100

    fix single submission

--- submit_scripts/create_foldseek_af50_struct_from_db_job_list.sh
+++ submit_scripts/create_foldseek_af50_struct_from_db_job_list.sh
@@ -7,5 +7,5 @@ hostname
 while IFS= read -r task_id; do
   # Execute the SUBMIT JOB command (replace it with the actual command)
     file_prefix=$(($task_id - 1))
-    qsub ~/profam/submit_scripts/create_foldseek_af50_struct_from_db_job_single.sh $file_prefix
+    qsub ~/profam/submit_scripts/create_foldseek_af50_struct_from_db_single.qsub.sh $file_prefix
 done < $1
