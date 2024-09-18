ced226bd6412a49eb1131da27da02ced62776a0e
alex-hh <alex.hhooker@gmail.com>
2024-09-19 00:49:26 +0100

    add lightweight llama model config for testing

--- tests/model/test_sampling.py
+++ tests/model/test_sampling.py
@@ -1,10 +1,10 @@
-def test_seqpos_start(default_model, proteingym_batch):
+def test_seqpos_start(test_model, proteingym_batch):
+    for n, p in test_model.named_parameters():
+        print(n, p.shape)
     # TODO: somehow use a dummy pipeline for sampling tests
     # n.b. completion seq pos is b, n, l
     assert (
         proteingym_batch["completion_seq_pos"][:, :, 1]
-        == default_model.model.start_seq_pos
+        == test_model.model.start_seq_pos
     ).all()
-    assert (
-        proteingym_batch["seq_pos"][:, 2] == default_model.model.start_seq_pos
-    ).all()
+    assert (proteingym_batch["seq_pos"][:, 2] == test_model.model.start_seq_pos).all()
