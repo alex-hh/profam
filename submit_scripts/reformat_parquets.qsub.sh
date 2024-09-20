ee408838bd680f0f1c349cab193e04c0e84ab904
alex-hh <alex.hhooker@gmail.com>
2024-09-20 02:31:03 +0100

    add foldseek no af50 struct creation script

---
+++ submit_scripts/reformat_parquets.qsub.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#$ -l tmem=15G
+#$ -l h_vmem=15G
+#$ -l h_rt=35:55:30
+#$ -S /bin/bash
+#$ -N foldseekF
+#$ -o /SAN/orengolab/cath_plm/ProFam/qsub_logs/
+#$ -wd ~/ProFam/ahh/profam
+#$ -j y
+date
+# conda activate venvPF
+source /share/apps/source_files/python/python-3.11.9.source
+source /SAN/orengolab/cath_plm/ProFam/pfenv/bin/activate
+DATA_DIR=/SAN/orengolab/cath_plm/ProFam/data python3 data_creation_scripts/reformat_parquets.py $1
+date
