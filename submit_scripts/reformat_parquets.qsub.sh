ec6f54cdd8b4c4e4c4b48bf42b1ffb872c5a98b6
alex-hh <alex.hhooker@gmail.com>
2024-09-21 16:34:17 +0100

    update reformat submit script

--- submit_scripts/reformat_parquets.qsub.sh
+++ submit_scripts/reformat_parquets.qsub.sh
@@ -3,13 +3,12 @@
 #$ -l h_vmem=35G
 #$ -l h_rt=35:55:30
 #$ -S /bin/bash
-#$ -N foldseekF
+#$ -N reformatFoldseek
 #$ -o /SAN/orengolab/cath_plm/ProFam/qsub_logs/
 #$ -wd ~/ProFam/ahh/profam
 #$ -j y
 date
 # conda activate venvPF
-source /share/apps/source_files/python/python-3.11.9.source
-source /SAN/orengolab/cath_plm/ProFam/pfenv/bin/activate
+source /SAN/orengolab/cath_plm/ProFam/pfenv.source
 DATA_DIR=/SAN/orengolab/cath_plm/ProFam/data python3 data_creation_scripts/reformat_parquets.py $1
 date
