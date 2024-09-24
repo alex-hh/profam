b551b70b8cb6d25850650e2db943aa1b4a9fd655
alex-hh <alex.hhooker@gmail.com>
2024-09-24 15:00:10 +0100

    save accession index

---
+++ submit_scripts/save_accession_index.qsub.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#$ -l tmem=48G
+#$ -l h_vmem=48G
+#$ -l h_rt=128:55:30
+#$ -S /bin/bash
+#$ -N builddb
+#$ -o /SAN/orengolab/cath_plm/ProFam/qsub_logs/
+#$ -wd ~/ProFam/ahh/profam
+#$ -j y
+date
+hostname
+# conda activate venvPF
+source /share/apps/source_files/python/python-3.11.9.source
+source /SAN/orengolab/cath_plm/ProFam/pfenv/bin/activate
+# source /share/apps/source_files/python/python-3.11.9.source
+python3 data_creation_scripts/foldseek/save_accession_index.py $@
+date
