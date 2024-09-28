710141e3a98e9c1d25a8d4252b0318e2a31a436d
judewells <judewells@hotmail.com>
2024-09-28 13:54:08 +0100

    resume train use scratch increase batch size

--- submit_scripts/resume_optimizer_test_train.qsub.sh
+++ submit_scripts/resume_optimizer_test_train.qsub.sh
@@ -12,7 +12,7 @@
 #$ -o /SAN/orengolab/cath_plm/ProFam/qsub_logs/
 #$ -wd /SAN/orengolab/cath_plm/ProFam/profam
 #$ -j y
-#$ -t 1-2
+#$ -t 1
 date
 hostname
 nvidia-smi
@@ -27,7 +27,9 @@ cd $ROOT_DIR
 conda activate venvPF
 export PYTHONPATH=$ROOT_DIR:$PYTHONPATH
 SCRATCH_DIR="/scratch0/${USER}/data"
+mkdir -p $SCRATCH_DIR
 export HF_HOME="/scratch0/${USER}/hf"
+mkdir -p $HF_HOME
 # Function to clean up temporary files
 cleanup() {
     echo "[$(date)] Cleaning up temporary files..."
@@ -40,6 +42,13 @@ trap cleanup EXIT ERR INT TERM
 PFAM_DIR="/SAN/orengolab/cath_plm/ProFam/data/pfam/train_test_split_parquets"
 GYM_DIR="/SAN/orengolab/cath_plm/ProFam/data/ProteinGym"
 mkdir -p $SCRATCH_DIR/pfam
+# check if scratch directory is created
+if [ -d $SCRATCH_DIR/pfam ]; then
+    echo "Scrat Pfam directory exists"
+else
+    echo "Directory does not exist"
+    exit 1
+fi
 rsync -av $PFAM_DIR $SCRATCH_DIR/pfam/
 rsync -av $GYM_DIR $SCRATCH_DIR/
 
@@ -55,6 +64,8 @@ else
     echo "Error: Invalid SGE_TASK_ID. Must be 1 or 2."
     exit 1
 fi
+
+
 echo "tree -d $SCRATCH_DIR:"
 tree -d $SCRATCH_DIR
 echo "ls ${SCRATCH_DIR}/pfam:"
