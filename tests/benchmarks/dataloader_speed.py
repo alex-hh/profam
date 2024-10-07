5634fe7557e213bb6494ed79e68ba7f95a7b4bb7
alex-hh <alex.hhooker@gmail.com>
2024-10-08 00:36:43 +0100

    improve benchmark script

--- tests/benchmarks/dataloader_speed.py
+++ tests/benchmarks/dataloader_speed.py
@@ -1,4 +1,8 @@
+import argparse
+import cProfile
+import io
 import os
+import pstats
 import time
 
 import hydra
@@ -6,26 +10,54 @@ from hydra import compose, initialize_config_dir
 
 from src.constants import BASEDIR
 
-with initialize_config_dir(os.path.join(BASEDIR, "configs")):
-    cfg = compose(
-        config_name="train",
-        overrides=[
-            "experiment=foldseek_inverse_folding",
-            "data=foldseek_interleaved",
-            f"paths.root_dir={BASEDIR}",
-        ],
-    )
-    tokenizer_cfg = compose(
-        config_name="tokenizer/profam",
-    )
 
+def main(max_iters: int):
+    pr = cProfile.Profile()
+    pr.enable()
 
-tokenizer = hydra.utils.instantiate(tokenizer_cfg.tokenizer)
-dm = hydra.utils.instantiate(cfg.data, tokenizer=tokenizer, _convert_="partial")
-dm.setup()
-train_loader = dm.train_dataloader()
+    with initialize_config_dir(os.path.join(BASEDIR, "configs")):
+        cfg = compose(
+            config_name="train",
+            overrides=[
+                "experiment=foldseek_inverse_folding",
+                "data=foldseek_interleaved",
+                "data.num_workers=0",
+                f"paths.root_dir={BASEDIR}",
+            ],
+        )
+        tokenizer_cfg = compose(
+            config_name="tokenizer/profam",
+        )
 
-t_prev = time.time()
-for ix, batch in enumerate(train_loader):
-    print(ix, time.time() - t_prev)
+    tokenizer = hydra.utils.instantiate(tokenizer_cfg.tokenizer)
+    dm = hydra.utils.instantiate(cfg.data, tokenizer=tokenizer, _convert_="partial")
+    dm.setup()
+    train_loader = dm.train_dataloader()
+
     t_prev = time.time()
+    for ix, batch in enumerate(train_loader):
+        if ix >= max_iters:
+            break
+        print(ix, time.time() - t_prev)
+        t_prev = time.time()
+
+    pr.disable()  # Stop profiling
+    pr.disable()
+
+    # Print profiling results
+    s = io.StringIO()
+    ps = pstats.Stats(pr, stream=s).sort_stats("cumulative")
+    ps.print_stats()
+    print(s.getvalue())
+
+    # Optionally, save profiling results to a file
+    ps.dump_stats(
+        os.path.join(BASEDIR, "tests", "benchmarks", "dataloader_profile.prof")
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("max_iters", type=int)
+    args = parser.parse_args()
+    main(args.max_iters)
