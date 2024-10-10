7b81759981e8ce83dd27aeab7cfb73d261c05f9e
alex-hh <alex.hhooker@gmail.com>
2024-10-10 18:35:17 +0100

    repeat rather than dropping leftover parquets"

--- src/data/datasets.py
+++ src/data/datasets.py
@@ -1,4 +1,5 @@
 import glob
+import math
 import os
 from dataclasses import dataclass
 from typing import List, Optional
@@ -108,6 +109,10 @@ def prepare_data_files(data_dir, cfg, world_size=1):
             )
         else:
             data_files = data_files[: (len(data_files) // world_size) * world_size]
+            leftover_files = data_files[len(data_files) // world_size * world_size :]
+            # worst case scenario is leftover_files=1: handle this or any other case by repeating maximal amount and slicing
+            repeated_leftovers = [leftover_files] * world_size
+            data_files = data_files + repeated_leftovers[: len(data_files) % world_size]
     return data_files
 
 
