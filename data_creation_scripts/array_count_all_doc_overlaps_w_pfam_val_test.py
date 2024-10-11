7ea13058ed66b4555953bd4e1bf4613cc5f0bb5b
judewells <judewells@hotmail.com>
2024-10-11 15:05:00 +0100

    fix div 0

--- data_creation_scripts/array_count_all_doc_overlaps_w_pfam_val_test.py
+++ data_creation_scripts/array_count_all_doc_overlaps_w_pfam_val_test.py
@@ -201,7 +201,7 @@ def process_dataset(counter_class, pfam_val_test, task_index, num_tasks, **kwarg
     
     overlap_counts = {}
     for i, (fam_id, up_ids) in enumerate(sorted(list(fam_id_up_ids.items()))[start_index:end_index], start=start_index):
-        if i % (fams_per_task // 10) == 0:
+        if i % max(1, (fams_per_task // 10)) == 0:
             logger.info(f"Processed {i-start_index+1}/{end_index-start_index} families")
         for pfam_fam, test_ids in pfam_val_test.items():
             intersection = set(up_ids).intersection(test_ids)
