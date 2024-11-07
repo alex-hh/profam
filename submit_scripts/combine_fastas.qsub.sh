23fffcceedfdb60b282de762026a3379a6c1bf22
judewells <judewells@hotmail.com>
2024-11-07 16:29:09 +0000

    combine fastas

---
+++ submit_scripts/combine_fastas.qsub.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#$ -l tmem=32G
+#$ -l h_vmem=32G
+#$ -l h_rt=92:55:30
+#$ -S /bin/bash
+#$ -N fastaCombi
+#$ -o /SAN/orengolab/cath_plm/ProFam/qsub_logs/
+#$ -wd /SAN/orengolab/cath_plm/ProFam/profam
+#$ -j y
+
+date
+hostname
+echo "#################### QSUB SCRIPT START ####################"
+cat "$0" # print the contents of this file to the log
+echo "#################### QSUB SCRIPT END  ####################"
+
+base_dir="/SAN/orengolab/cath_plm/ProFam/data/uniprot"
+# Define input and output paths
+input1="${base_dir}/uniprot_sprot.fasta.gz"
+input2="${base_dir}/uniprot_trembl.fasta.gz"
+output="${base_dir}/combined_sprot_trembl_uniprot.fasta"
+
+# Unzip, extract accessions, and combine both files
+{
+    zcat "$input1" | awk '/^>/{print ">" substr($1, 5, index($1, "|", 5) - 5)} !/^>/{print}'
+    zcat "$input2" | awk '/^>/{print ">" substr($1, 5, index($1, "|", 5) - 5)} !/^>/{print}'
+} > "$output"
+
+echo "Combined FASTA file created: $output"
