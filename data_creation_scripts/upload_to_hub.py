a8eb5136cd1597737d7f1c7c710ecfba4d62f0e0
alex-hh <alex.hhooker@gmail.com>
2024-11-09 16:02:42 +0000

    add config/split info to hf hub uploads

--- data_creation_scripts/upload_to_hub.py
+++ data_creation_scripts/upload_to_hub.py
@@ -5,9 +5,11 @@ from huggingface_hub import HfApi
 
 def main(args):
     api = HfApi()
+    path_in_repo = f"{args.config_name}/{args.split_name}"
     api.upload_folder(
         folder_path=args.data_dir,
         repo_id=args.repo_id,
+        path_in_repo=path_in_repo,
     )
 
 
@@ -15,5 +17,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--repo-id", type=str, required=True)
     parser.add_argument("--data-dir", type=str, required=True)
+    # determine repo organization
+    # https://huggingface.co/docs/datasets/en/repository_structure
+    # makes sense to have a folder for each config and split (config_name/split_name)
+    # Your data files may also be placed into different directories named train, test, and validation where each directory contains the data files for that split:
+    parser.add_argument("--config-name", type=str, required=True)
+    parser.add_argument("--split-name", type=str, default="train")
     args = parser.parse_args()
     main(args)
