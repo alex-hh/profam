8a148c1e1683c56623fa0e4092f5f0531963b345
alex-hh <alex.hhooker@gmail.com>
2024-11-10 06:44:04 +0000

    use upload_large_folder

--- data_creation_scripts/upload_to_hub.py
+++ data_creation_scripts/upload_to_hub.py
@@ -1,17 +1,30 @@
-"""We can upload parquet files to the hub directly."""
+"""We can upload parquet files to the hub directly.
+
+Use hf_transfer: this is a Rust-based library meant to speed up uploads on machines with very high bandwidth. To use hf_transfer:
+Specify the hf_transfer extra when installing huggingface_hub (i.e., pip install huggingface_hub[hf_transfer]).
+Set HF_HUB_ENABLE_HF_TRANSFER=1 as an environment variable.
+"""
 import argparse
 from huggingface_hub import HfApi
 
 
 def main(args):
+    # https://huggingface.co/docs/huggingface_hub/en/guides/upload#upload-a-folder-by-chunks
     api = HfApi()
     path_in_repo = f"{args.config_name}/{args.split_name}"
-    api.upload_folder(
+    # TODO: use upload_large_folder (requires correct local directory structure since path_in_repo not supported)
+    api.upload_large_folder(
         folder_path=args.data_dir,
         repo_id=args.repo_id,
-        path_in_repo=path_in_repo,
         repo_type="dataset",
     )
+    # api.upload_folder(
+    #     folder_path=args.data_dir,
+    #     repo_id=args.repo_id,
+    #     path_in_repo=path_in_repo,
+    #     repo_type="dataset",
+    #     multi_commits=True,
+    # )
 
 
 if __name__ == "__main__":
