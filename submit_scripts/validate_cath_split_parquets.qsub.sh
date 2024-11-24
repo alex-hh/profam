432e91a3bba444591b77a17f165905c00f4eb5d4
judewells <judewells@hotmail.com>
2024-11-24 16:40:08 +0000

    script for validating new parquets

---
+++ submit_scripts/validate_cath_split_parquets.qsub.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#$ -l tmem=192G
+#$ -l h_vmem=192G
+#$ -l h_rt=71:55:30
+#$ -S /bin/bash
+#$ -N checkParqs
+#$ -o /SAN/orengolab/cath_plm/ProFam/qsub_logs/
+#$ -wd /SAN/orengolab/cath_plm/ProFam/profam
+#$ -j y
+
+date
+hostname
+echo "#################### QSUB SCRIPT START ####################"
+cat "$0" # print the contents of this file to the log
+echo "#################### QSUB SCRIPT END  ####################"
+conda activate venvPF
+ROOT_DIR='/SAN/orengolab/cath_plm/ProFam/profam'
+cd $ROOT_DIR
+
+# Add ROOT_DIR to PYTHONPATH
+export PYTHONPATH=$ROOT_DIR:$PYTHONPATH
+python scripts/adhoc_analysis/validate_cath_split_parquets.py
+date
