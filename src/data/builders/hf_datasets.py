c3e4e457b5d8be318e03c6b90b7a90769660152b
judewells <judewells@hotmail.com>
2024-11-25 12:21:06 +0000

    fix bug

--- src/data/builders/hf_datasets.py
+++ src/data/builders/hf_datasets.py
@@ -226,7 +226,7 @@ class FileBasedHFProteinDataset(BaseProteinDataset):
         )  # formatting needs to be set before filter/map
 
         if self.streaming:
-            print(f"Dataset n shards: {dataset.num_shards}")
+            print(f"Dataset n shards: {dataset.n_shards}")
         else:
             print(f"Loaded dataset {self.name} with {len(dataset)} samples")
         if verbose:
