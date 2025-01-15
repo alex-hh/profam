38fa238d0c5e80c5b1c10a100b5f08afa48ae143
judewells <judewells@hotmail.com>
2025-01-15 18:12:19 +0000

    update configs

--- submit_scripts/train_funfam_foldseek_ted.qsub.sh
+++ submit_scripts/train_funfam_foldseek_ted.qsub.sh
@@ -2,13 +2,13 @@
 
 # Train ProFam
 
-#$ -l tmem=127.9G
-#$ -l h_vmem=127.9G
+#$ -l tmem=253G
+#$ -l h_vmem=253G
 #$ -l gpu=true
 #$ -l gpu_type=(a100|a100_80)
 #$ -l h_rt=71:55:30
 #$ -S /bin/bash
-#$ -N v2_ff_fs_ted
+#$ -N v3_ff_fs_ted
 #$ -o /SAN/orengolab/cath_plm/ProFam/qsub_logs/
 #$ -wd /SAN/orengolab/cath_plm/ProFam/profam
 #$ -j y
@@ -27,7 +27,7 @@ experiment=train_funfams_ted_no_s100_foldseek_no_struct \
 trainer=gpu \
 logger=wandb \
 data.pack_to_max_tokens=140_000 \
-data.num_workers=32 \
+data.num_workers=24 \
 trainer.val_check_interval=2500 \
 ckpt_path=/SAN/orengolab/cath_plm/ProFam/profam/logs/train_ff_fs_ted_pg/runs/2025-01-13_12-07-44-967554/checkpoints/last.ckpt
 date
