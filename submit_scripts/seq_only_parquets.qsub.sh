f431078501660590a7afcbc659504b0bcf3463a1
judewells <judewells@hotmail.com>
2025-01-18 13:30:22 +0000

    fs50 seq only

--- submit_scripts/seq_only_parquets.qsub.sh
+++ submit_scripts/seq_only_parquets.qsub.sh
@@ -3,7 +3,7 @@
 #$ -l h_vmem=32G
 #$ -l h_rt=3:55:30
 #$ -S /bin/bash
-#$ -N seq_only_parquets
+#$ -N seq_parqs_fsS50
 #$ -t 20
 #$ -o /SAN/orengolab/cath_plm/ProFam/qsub_logs/
 #$ -wd /SAN/orengolab/cath_plm/ProFam/profam
@@ -13,6 +13,6 @@ conda activate venvPF
 python data_creation_scripts/seq_only_parquets.py \
 --task_index $((SGE_TASK_ID - 1)) \
 --num_tasks $SGE_TASK_LAST \
---parquet_dir /SAN/orengolab/cath_plm/ProFam/data/foldseek/foldseek_s100_raw/train_val_test_split \
---new_parquet_dir /SAN/orengolab/cath_plm/ProFam/data/foldseek/foldseek_s100_raw/train_val_test_split_seq_only
+--parquet_dir /SAN/orengolab/cath_plm/ProFam/data/foldseek/foldseek_s50_struct/train_val_test_split \
+--new_parquet_dir /SAN/orengolab/cath_plm/ProFam/data/foldseek/foldseek_s50_seq_only/train_val_test_split
 date
