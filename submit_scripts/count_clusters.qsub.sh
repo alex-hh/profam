19d105f8e7716176554525432ff1ca116954a082
judewells <judewells@hotmail.com>
2025-02-17 10:24:15 +0000

    count clusters

--- submit_scripts/count_clusters.qsub.sh
+++ submit_scripts/count_clusters.qsub.sh
@@ -2,12 +2,12 @@
 
 #$ -l tmem=7.9G
 #$ -l h_vmem=7.9G
-#$ -l h_rt=23:55:30
+#$ -l h_rt=3:55:30
 #$ -S /bin/bash
-#$ -N clusterFamilies
+#$ -N countClusters
 #$ -o /SAN/orengolab/cath_plm/ProFam/qsub_logs/clustering/
 #$ -wd /SAN/orengolab/cath_plm/ProFam/profam
-#$ -t 1-200
+#$ -t 1-50
 #$ -j y
 
 date
@@ -18,8 +18,8 @@ echo "#################### QSUB SCRIPT END  ####################"
 conda activate venvPF
 export ROOT_DIR='/SAN/orengolab/cath_plm/ProFam/profam'
 export PYTHONPATH=$ROOT_DIR:$PYTHONPATH
-python /SAN/orengolab/cath_plm/ProFam/profam/data_creation_scripts/cluster_within_families.py \
-    --input_pattern "/SAN/orengolab/cath_plm/ProFam/data/ted/s100_parquets/train_val_test_split_hq/train_val_test_split/*/*.parquet" \
+python /SAN/orengolab/cath_plm/ProFam/profam/data_creation_scripts/count_clusters.py \
+    --input_pattern "../data/ted/s100_parquets/train_val_test_split_hq/train_val_test_split/*/clustered/*.parquet" \
     --task_index $((SGE_TASK_ID - 1)) \
     --num_tasks $SGE_TASK_LAST
 date
