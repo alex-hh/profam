809a3e6d576c297b8b06b2da773c60e18f726209
judewells <judewells@hotmail.com>
2025-03-24 22:11:02 +0000

    fix open fold msas

--- submit_scripts/openfold_process_msa_fragments.qsub.sh
+++ submit_scripts/openfold_process_msa_fragments.qsub.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#$ -l tmem=32G
-#$ -l h_vmem=32G
-#$ -l h_rt=23:55:30
+#$ -l tmem=16G
+#$ -l h_vmem=16G
+#$ -l h_rt=47:55:30
 #$ -S /bin/bash
 #$ -l tscratch=10G
 # hostname does not contain larry* or arbuckle* or abner*
 #$ -l hostname=!(larry*|arbuckle*|abner*)
-#$ -N openfoldCluster
-#$ -o /SAN/orengolab/cath_plm/ProFam/qsub_logs/clusteringV2/
+#$ -N OFcluster
+#$ -o /SAN/orengolab/cath_plm/ProFam/qsub_logs/openfold_clustering/
 #$ -wd /SAN/orengolab/cath_plm/ProFam/profam
 #$ -t 1-270
 #$ -j y
