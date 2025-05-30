d08f2a860dc2bca57edf7b5829f181d461c02439
judewells <judewells@hotmail.com>
2025-05-30 12:15:04 +0100

    adjust gradient accumulation schedule

--- src/models/base.py
+++ src/models/base.py
@@ -1103,6 +1103,12 @@ class BaseFamilyLitModule(BaseLitModule):
             prog_bar=True,
             on_epoch=False,
         )
+        self.log(
+            "train/accumulate_grad_batches",
+            self.trainer.accumulate_grad_batches,
+            on_step=True,
+            on_epoch=False,
+        )
         return loss
 
     def on_train_epoch_end(self):
