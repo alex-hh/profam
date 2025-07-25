011a2d049d14e8045413f882a2eafb8ffa095b59
judewells <judewells@hotmail.com>
2025-07-26 00:21:07 +0100

    save all likelihoods

--- submit_scripts/evaluate_all_gym_models.qsub.sh
+++ submit_scripts/evaluate_all_gym_models.qsub.sh
@@ -73,15 +73,15 @@ echo "Experiment group : $NAME"
 echo "Running evaluation..."
 
 python src/train.py \
-  experiment_group="$NAME" \
-  --config-dir="$DIR/.hydra" \
-  --config-name=config_gym.yaml \
-  model.config.scoring_max_tokens=120_000 \
-  train=false \
-  test=true \
-  data.dataset_builders.proteingym.max_tokens_per_example=2_000_000 \
-  data.dataset_builders.proteingym.dms_ids: null \
-  data.dataset_builders.proteingym.max_mutated_sequences=3000 \
-  ckpt_path="$DIR/checkpoints/last.ckpt"
+experiment_group="${NAME}" \
+--config-dir="${DIR}/.hydra" \
+--config-name=config_gym.yaml \
+model.config.scoring_max_tokens=120_000 \
+train=false \
+test=true \
+data.dataset_builders.proteingym.max_tokens_per_example=2000000 \
+data.dataset_builders.proteingym.dms_ids=null \
+data.dataset_builders.proteingym.max_mutated_sequences=3000 \
+ckpt_path="${DIR}/checkpoints/last.ckpt"
 
 date
