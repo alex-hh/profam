813a8166487bd595c68cbcd42ba9ec758a02f069
judewells <judewells@hotmail.com>
2025-08-31 14:38:35 +0100

    gym sampling strategy v9

--- scripts/adhoc_analysis/protein_gym_likelihood_how_does_n_samples_with_selection_affect_performance.py
+++ scripts/adhoc_analysis/protein_gym_likelihood_how_does_n_samples_with_selection_affect_performance.py
@@ -6,9 +6,13 @@ import matplotlib.pyplot as plt
 from scipy.stats import spearmanr
 
 """
-Created by Jude Wells 2025-08-04
-Generates plots showing the number of ensembed predictions and 
-the spearman correlation.
+Created by Jude Wells 2025-08-30
+Answers a different question to:
+scripts/adhoc_analysis/protein_gym_likelihood_n_samples_plot copy.py
+
+In the former script we always selected the top-scoring from all 200 forward passes
+In this script we ask the question: what if we had n < 200 forward passes to select from?
+This script should inform us how much benefit we get from increasing the number of forward passes.
 """
 
 def load_dms_scores(csv_path, seed=42, max_mutated_sequences=3000):
@@ -38,57 +42,51 @@ if __name__ == "__main__":
     # npz_files = glob.glob("logs/proteingym_eval_results/20250808_000300_PoET_MSAs_BO/*.npz")
     # npz_files = glob.glob("logs/proteingym_eval_results/20250810_135739_poet_msas_random_sampling_v4/*.npz")
     # npz_files = glob.glob("logs/abyoeovl_openfold_fs50_ur90_memmap_251m_copied_2025-06-23_22-18/20250810_135739_full_gym/*.npz")
-    # npz_files = glob.glob("logs/abyoeovl_openfold_fs50_ur90_memmap_251m_copied_2025-06-23_22-18/20250810_135739_v6_full_gym/*v6_lls.npz")
+    npz_files = glob.glob("logs/abyoeovl_openfold_fs50_ur90_memmap_251m_copied_2025-06-23_22-18/20250810_135739_v6_full_gym/*v6_lls.npz")
     # npz_files = glob.glob("logs/abyoeovl_openfold_fs50_ur90_memmap_251m_copied_2025-06-23_22-18/20250828_v6_gym_msas/*v6_lls.npz")
     # npz_files = glob.glob("logs/abyoeovl_openfold_fs50_ur90_memmap_251m_copied_2025-06-23_22-18/20250829_v6_gym_msas_filtered/*v6_lls.npz")
-    npz_files = glob.glob("logs/abyoeovl_openfold_fs50_ur90_memmap_251m_copied_2025-06-23_22-18/20250829_msa_pariformer_exp_2_gym_msas_unfiltered/*v7_lls.npz")
-    save_dir = "../gym_spearman_likelihood_plots_full_gym_msas_no_filter_test"
+    save_dir = "../v6_gym_msas_evaluating_n_forward"
     os.makedirs(save_dir, exist_ok=True)
-    target_scores = [-1.3]
-    for target_score in target_scores:
-        print(f"Found {len(npz_files)} npz files")
-        results_rows = []
-        completed_dms_ids = []
-        samps_used = []
-        spearman_corrs = []
-        sorted_spearman_corrs = []
-        for i in [1, 2, 5, 10, 20, 30, 50, 80, 120, 160, 200]:
-            print(f"Processing {i} samples")
+    results_rows = []
+    target_score = -1.3
+    for n_forward in [10, 20, 50, 100, 200]:
+        for top_pct in [1.0, 0.7, 0.5, 0.3, 0.2]:
+            spearman_scores = []
+            print(f"Found {len(npz_files)} npz files")
+            n_select = int(n_forward * top_pct)
+            print(f"Processing {n_select} samples")
             one_value_spearman_corrs = []
             one_value_spearman_corrs_sorted = []
             for npz_file in npz_files:
-                npz_file = npz_file.replace("20250829_msa_pariformer_exp_2_gym_msas_unfiltered", "20250828_v6_gym_msas").replace("v7_lls.npz", "v6_lls.npz")
                 data = np.load(npz_file)
                 dms_scores = data["dms_scores"]
                 assert len(dms_scores) == data["lls"].shape[1], "Number of lls and dms scores must match"
                 if data["lls"].shape[0] < 200:
                     print(f"Skipping {npz_file} because it has {data['lls'].shape[0]} reps")
                     continue
-                lls_to_use = data["lls"][:i]
-                lls_mean = lls_to_use.mean(axis=0)
-                one_value_spearman_corrs.append(spearmanr(lls_mean, dms_scores)[0])
+                lls_to_use = data["lls"][:n_forward]
                 # sort the lls by the mean likelihood
-                mutant_means = data["lls"].mean(axis=1)
+                mutant_means = lls_to_use.mean(axis=1)
                 lls_distances = np.abs(mutant_means - target_score)
                 lls_mean_sorted_indices = np.argsort(lls_distances)
-                lls_sorted = data["lls"][lls_mean_sorted_indices]
-                lls_to_use_sorted = lls_sorted[:i]
-
+                lls_sorted = lls_to_use[lls_mean_sorted_indices]
+                lls_to_use_sorted = lls_sorted[:n_select]
                 lls_mean_sorted = lls_to_use_sorted.mean(axis=0)
-                one_value_spearman_corrs_sorted.append(spearmanr(lls_mean_sorted, dms_scores)[0])
-
-            samps_used.append(i)
-            spearman_corrs.append(np.mean(one_value_spearman_corrs))
-            sorted_spearman_corrs.append(np.mean(one_value_spearman_corrs_sorted))
-
-        plt.plot(samps_used, sorted_spearman_corrs, label=f"Sorted {target_score}")
-        if target_score == target_scores[0]:
-            plt.plot(samps_used, spearman_corrs, label="Unsorted", color="blue")
-        plt.xlabel("Number of samples used")
-        plt.ylabel("Spearman correlation")
-        plt.title("Spearman correlation vs. number of samples used")
-        plt.legend()
-        plt.savefig(f"{save_dir}/protein_gym_likelihood_n_samples_plot_{target_score}.png")
+                spearman_score = spearmanr(lls_mean_sorted, dms_scores)[0]
+                spearman_scores.append(spearman_score)
+            mean_spearman_score = np.mean(spearman_scores)
+            new_row = {
+                "n_forward": n_forward,
+                "top_pct": top_pct,
+                "n_select": n_select,
+                "target_score": target_score,
+                "spearman_score": mean_spearman_score,
+            }
+            results_rows.append(new_row)
+    results_df = pd.DataFrame(results_rows)
+    print(results_df.groupby("n_forward").max())
+    # results_df.to_csv(f"{save_dir}/protein_gym_likelihood_how_does_n_samples_with_selection_affect_performance.csv", index=False)
+                
 
 
 
